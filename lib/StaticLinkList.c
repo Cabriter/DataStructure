bc3f5a500acc37a140f7ee1e166ada52135e6592
Cabriter <18669770359@163.com>
2016-05-12 09:16:06 +0800

    add staticLinkList inster delete CalcLength and FreeNode

--- lib/StaticLinkList.c
+++ lib/StaticLinkList.c
@@ -18,11 +18,7 @@ int ListLength(StaticLinkList L);
 
 int main()
 {
-
-
-
-
-
+	//test code
 
 	return 1;
 }
@@ -46,4 +42,62 @@ int Malloc_SLL(StaticLinkList space)
 	  space[0].cur = space[i].cur;
 
 	return i;
+}
+
+int ListInstert(StaticLinkList L,int i,int e)
+{
+	int j,k,l;
+	k = MAX_SIZE - 1;
+	if(i < 1 || i > ListLength(L) + 1)
+	  return 0;
+	j = Malloc_SSL(L);
+	if(j)
+	{
+		L[j].data = e;
+		for(l = 1;l <= i - 1;l++)
+		{
+			k = L[k].cur;
+		}
+		L[j].cur = L[k].cur;
+		L[k].cur = j;
+		return 1;
+	}
+	return 0;
+}
+
+int ListDelete(StaticLinkList L,int i)
+{
+	int j,k;
+	if(i < 1||i > ListLength(L))
+	{
+		return 0;
+	}
+	k = MAX_SIZE - 1;
+	for(j = 1;j < = i - 1;j++)
+	{
+		k = L[k].cur;
+	}
+	j = L[k].cur;
+	L[k].cur = L[j].cur;
+	Free_SSL(L,j);
+	return 1;
+
+}
+
+int Free_SSL(StaticLinkList space,int k)
+{
+	space[k].cur = space[0].cur;
+	space[0].cur = k;
+}
+
+int ListLength(StaticLinkList L)
+{
+	int j = 0;
+	int i = L[MAXSIZE - 1].cur;
+	while(i)
+	{
+		i = L[i].cur;
+		j++;
+	}
+	return j;
 }
