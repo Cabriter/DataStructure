7f579c1bff3411d2a0061051532b10e9a41896a4
Cabriter <18669770359@163.com>
2016-05-14 09:27:25 +0800

    write DoubleLinkList

--- lib/DoubleLinkList.c
+++ lib/DoubleLinkList.c
@@ -1,5 +1,20 @@
 #include "stdio.h"
 
+typedef struct DulNode
+{
+	int data;
+	struct DulData *prior;
+	struct DulData *next;
+}DulNode,*DuLinkList;
+
+int DulListCreate();
+int DulListInsert();
+int DulListDelete();
+int DulListSet();
+int DulListFind();
+int CalcDulListLength();
+
+
 int main()
 {
 
